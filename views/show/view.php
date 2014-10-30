4b08bddd6223a192be4fe955bf5908ccdd039c2f
Florian Bieringer <florian.bieringer@uni-passau.de>
2014-10-30 10:39:50 +0100

    Fix lines

--- views/show/view.php
+++ views/show/view.php
@@ -1,4 +1,4 @@
 <h3><?= htmlReady($log->name) ?></h3>
-<? foreach ($log->file as $row): ?>
-    <p class="entry"><?= htmlReady($row) ?><p>
-    <? endforeach; ?>
+<? foreach (array_slice($log->file, 0, 1000) as $row): ?>
+    <p class="entry"><?= htmlReady($row) ?></p>
+<? endforeach; ?>
